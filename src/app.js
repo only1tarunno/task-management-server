b4438ab9d4c277739f3b8f2aa3f80c600158feb8
only1tarunno <tarunno281@gmail.com>
2023-12-20 21:13:24 +0600

    server create

---
+++ src/app.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+const express = require("express");
+const applyMiddleWares = require("./middleWares/applyMiddleWares");
+const app = express();
+
+// routers
+const authRoutes = require("./routes/Authentication/authentication");
+
+// middlewares
+applyMiddleWares(app);
+
+// jwt related api
+app.use(authRoutes);
+
+// server health check api
+app.get("/", (req, res) => {
+  res.send("server is running");
+});
+
+// handling all (get,post,update,delete.....) unhandled routes
+app.all("*", (req, res, next) => {
+  const error = new Error(`Can't find [${req.originalUrl}] on the server`);
+  error.status = 404;
+  next(error);
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    message: err.message,
+    errors: err.errors,
+  });
+});
+
+module.exports = app;
