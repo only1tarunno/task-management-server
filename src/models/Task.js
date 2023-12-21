3a1abb8611b009ec7e49e4f6bd843a29ffde921f
only1tarunno <tarunno281@gmail.com>
2023-12-21 10:50:16 +0600

    task model create

---
+++ src/models/Task.js
@@ -0,0 +1,23 @@
+const { Schema, model } = require("mongoose");
+
+const TaskSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  deadline: {
+    type: String,
+    required: true,
+  },
+  priority: {
+    type: String,
+    required: true,
+  },
+});
+
+const Task = model("Task", TaskSchema);
+module.exports = Task;
